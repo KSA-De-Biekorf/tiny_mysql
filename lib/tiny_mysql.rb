f9a855215ebb0341a17584297c5bc99af4868a10
Genki Sugawara <sugawara@cookpad.com>
2017-03-03 16:56:15 +0900

    add implementation

--- lib/tiny_mysql.rb
+++ lib/tiny_mysql.rb
@@ -1,5 +1,49 @@
-require "tiny_mysql/version"
+require 'tiny_mysql/version'
 
-module TinyMysql
-  # Your code goes here...
+class TinyMysql
+  attr_reader :mysql_command
+  attr_reader :defaults_extra_file
+  attr_reader :host
+  attr_reader :port
+  attr_reader :username
+  attr_reader :database
+
+  def initialize(options = {})
+    @mysql_command = options[:mysql_command] || 'mysql'
+    @defaults_extra_file = options[:defaults_extra_file]
+    @host = options[:host] || 'localhost' unless @defaults_extra_file
+    @port = options[:port]
+    @username = options[:username] || 'root' unless @defaults_extra_file
+    @password = options[:password]
+    @database = options[:database]
+  end
+
+  def query(sql)
+    out, err, status = Open3.capture3(*cmd_with_args, :stdin_data => sql)
+    status.success? or raise err
+    parse(out)
+  end
+
+  private
+
+  def cmd_with_args
+    list = []
+    list << {'MYSQL_PWD' => @password} if @password
+    list << @mysql_command
+    list << "--defaults-extra-file=#{@defaults_extra_file}" if @defaults_extra_file
+    list << '-h' << @host if @host
+    list << '-P' << @port if @port
+    list << '-u' << @username if @username
+    list << @database if @database
+    list
+  end
+
+  def parse(out)
+    out = out.split("\n")
+    columns = (out.shift || '').split("\t")
+
+    out.map do |row|
+      Hash[columns.zip(row.split("\t"))]
+    end
+  end
 end
--- lib/tiny_mysql/version.rb
+++ lib/tiny_mysql/version.rb
@@ -1,3 +1,3 @@
-module TinyMysql
-  VERSION = "0.1.0"
+class TinyMysql
+  VERSION = '0.1.0'
 end
